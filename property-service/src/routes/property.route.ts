92d03c839d75bf41e1eaf94ab5aca44c1aa815ad
Hà Anh Thảo <thaoanhhaa1@gmail.com>
2024-07-29 23:59:18 +0700

    Deploy to vercel

--- property-service/src/routes/property.route.ts
+++ property-service/src/routes/property.route.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import upload from '../configs/multer.config';
-import { createProperty, getAllProperties, getPropertyBySlug } from '../controllers/property.controller';
+import {
+    createProperty,
+    getAllProperties,
+    getPropertyBySlug,
+    searchProperties,
+} from '../controllers/property.controller';
 import authMiddleware from '../middlewares/auth.middleware';
 import roleMiddleware from '../middlewares/role.middleware';
 
 const router = express.Router();
 
+router.get('/search', searchProperties);
 router.get('/slug/:slug', getPropertyBySlug);
 router.post('/', authMiddleware, roleMiddleware('owner'), upload.array('images'), createProperty);
 router.get('/', getAllProperties);
