aab2dcc2495221f440ee90efccc16af92a200e25
NguyenHieu09 <hieu92145@gmail.com>
2024-09-10 09:39:33 +0700

    [CHORE] envConfig

--- contract-service/src/tasks/agenda.ts
+++ contract-service/src/tasks/agenda.ts
@@ -1,14 +1,15 @@
 import { Agenda, Job } from 'agenda';
 import { endContract } from '../repositories/contract.repository';
+import envConfig from '../configs/env.config';
 import { checkOverduePayments } from './checkOverduePayments'; // Đảm bảo rằng bạn đã tạo file này
 import prisma from '../prisma/prismaClient';
-const mongoConnectionString = 'mongodb+srv://hieu92145:17012002@cluster0.nxsfo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; // Thay đổi URL này thành URL kết nối MongoDB của bạn
+const mongoConnectionString = envConfig.MONGO_CONNECTION;
 
 const agenda = new Agenda({ db: { address: mongoConnectionString, collection: 'agendaJobs' } });
 
 agenda.define('end contracts', async (job, done) => {
     console.log('Running job: end contracts');
-    
+
     // Lấy danh sách các hợp đồng cần kiểm tra
     const contracts = await prisma.contract.findMany({
         where: {
@@ -31,7 +32,7 @@ agenda.define('end contracts', async (job, done) => {
 
 agenda.define('check overdue payments', async (job, done) => {
     console.log('Running job: check overdue payments');
-    
+
     try {
         await checkOverduePayments();
     } catch (error) {
@@ -44,18 +45,18 @@ agenda.define('check overdue payments', async (job, done) => {
 export const startAgenda = async () => {
     await agenda.start();
 
-   // Lên lịch công việc kiểm tra quá hạn thanh toán mỗi ngày vào lúc 00:00
-   await agenda.every('0 0 * * *', 'check overdue payments');
-   console.log('Scheduled job: check overdue payments');
+    // Lên lịch công việc kiểm tra quá hạn thanh toán mỗi ngày vào lúc 00:00
+    await agenda.every('0 0 * * *', 'check overdue payments');
+    console.log('Scheduled job: check overdue payments');
 
-   // Lên lịch công việc kết thúc hợp đồng mỗi ngày vào lúc 01:00
-   await agenda.every('0 1 * * *', 'end contracts');
-   console.log('Scheduled job: end contracts');
+    // Lên lịch công việc kết thúc hợp đồng mỗi ngày vào lúc 01:00
+    await agenda.every('0 1 * * *', 'end contracts');
+    console.log('Scheduled job: end contracts');
 
-   // Thực thi công việc ngay khi ứng dụng khởi động
-   await agenda.now('check overdue payments', {});
-   console.log('Executed job: check overdue payments');
+    // Thực thi công việc ngay khi ứng dụng khởi động
+    await agenda.now('check overdue payments', {});
+    console.log('Executed job: check overdue payments');
 
-   await agenda.now('end contracts', {});
-   console.log('Executed job: end contracts');
+    await agenda.now('end contracts', {});
+    console.log('Executed job: end contracts');
 };
