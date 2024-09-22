d9e74dbbafd0a4be5dedae577bbf906bc79a7222
Hà Anh Thảo <thaoanhhaa1@gmail.com>
2024-09-23 00:08:20 +0700

    [FIX] Contract Service - Call API from coingecko

--- contract-service/src/services/coingecko.service.ts
+++ contract-service/src/services/coingecko.service.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import envConfig from '../configs/env.config';
 import Redis from '../configs/redis.config';
 import { IGetCoinPrice } from '../interfaces/coingecko';
@@ -9,31 +10,20 @@ export const getCoinPriceService = async ({ coin, currency }: IGetCoinPrice) => 
         if (res) return parseFloat(res);
 
         const url = `${envConfig.COINGECKO_ENDPOINT}/simple/price?ids=${coin}&vs_currencies=${currency}`;
-        const options = {
-            method: 'GET',
-            // headers: { accept: 'application/json', 'x-cg-demo-api-key': envConfig.COINGECKO_API_KEY },
-        };
+        // const options = {
+        //     method: 'GET',
+        //     // headers: { accept: 'application/json', 'x-cg-demo-api-key': envConfig.COINGECKO_API_KEY },
+        // };
 
-        const response = await fetch(url, options);
-        console.log('🚀 ~ getCoinPriceService ~ response:', response);
-        const data = await response.text();
-        console.log('🚀 ~ getCoinPriceService ~ data:', data);
+        // const response = await fetch(url, options);
+        // console.log('🚀 ~ getCoinPriceService ~ response:', response);
+        // const data = await response.text();
+        // console.log('🚀 ~ getCoinPriceService ~ data:', data);
 
-        // Redis.getInstance()
-        //     .getClient()
-        //     .set(`coin-eth-vnd`, data?.[coin]?.[currency] ?? 0, {
-        //         ex: 60, // 1 phút
-        //         type: 'double',
-        //     })
-        //     .then(() => {
-        //         console.log('Set redis cache');
-        //     })
-        //     .catch((err: any) => {
-        //         console.error('Error setting redis cache:', err);
-        //     });
+        const test = await axios.get(url);
+        console.log('🚀 ~ getCoinPriceService ~ test:', test.data);
 
-        // return data?.[coin]?.[currency] ?? 0;
-        return 0;
+        return test.data[coin][currency];
     } catch (error) {
         console.error('Error getCoinPriceService:', error);
         throw error;
